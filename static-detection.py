7402290c6d4790ceb9964de965aa127991c32b59
soggy-soup <odion1776@gmail.com>
2024-01-17 17:48:50 -0500

    window sizing

--- static-detection.py
+++ static-detection.py
@@ -7,7 +7,21 @@ class process_img:
         self.img_in_processing = self.img_read
 
     def img_show(self):
-        cv2.imshow("Image in Processing", self.img_in_processing)
+        aspect_ratio = self.img_in_processing.shape[1] / self.img_in_processing.shape[0]
+        cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
+        cv2.imshow("Display Image", self.img_in_processing)
+        
+        while True:
+            key = cv2.waitKey(1)
+            if key == 27:  # Break the loop if 'Esc' key is pressed
+                break
+
+            current_width = cv2.getWindowImageRect("Display Image")[2]
+            new_height = int(current_width / aspect_ratio)
+            cv2.resizeWindow("Display Image", current_width, new_height)
+        cv2.destroyAllWindows()
+
+
 
     def img_scale(self, percent = None):
         width = int(self.img_in_processing.shape[1] * percent/100)
@@ -17,17 +31,17 @@ class process_img:
 
 
 
-    def img_crop(self,):
-
-a
-    def img_detect_contours(self,):
+    #def img_crop(self,):
 
 
-    def img_draw_contours(self,):
+   # def img_detect_contours(self,):
 
 
+    #def img_draw_contours(self,):
 
+test = process_img('C:/Users/odion/OneDrive/Documents/CAD Stuff/CNC Welding Table/New Computer Vision Code/computervisioncontrolledcncweldingmachine/images/othersideofplate.jpg')
 
+test.img_show()
 
 
 
