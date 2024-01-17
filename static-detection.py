3fef92332b6c612722b9cce216665d539fa495b5
soggy-soup <odion1776@gmail.com>
2024-01-17 18:32:37 -0500

    update

--- static-detection.py
+++ static-detection.py
@@ -25,17 +25,21 @@ class process_img:
         
     def img_crop(self):
         self.img_in_processing = self.img_in_processing[175:3300, 535:3950]
-        
+    
+    def img_gaussian_blur(self):
+        self.img_in_processing = cv2.GaussianBlur(self.img_in_processing, (101, 101), 5)
+
     def img_detect_contours(self,):
         gray = cv2.cvtColor(self.img_in_processing, cv2.COLOR_RGB2GRAY)
         _, thresh = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY)
         self.contours, self.heirarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     
     def img_draw_contours(self,):
-        self.img_in_processing = cv2.drawContours(self.img_in_processing, self.contours, -1, (0, 255, 0), 30)
+        self.img_in_processing = cv2.drawContours(self.img_read[175:3300, 535:3950], self.contours, -1, (0, 255, 0), 30)
 
 test = process_img('C:/Users/odion/OneDrive/Documents/CAD Stuff/CNC Welding Table/New Computer Vision Code/computervisioncontrolledcncweldingmachine/images/othersideofplate.jpg')
 test.img_crop()
+test.img_gaussian_blur()
 test.img_detect_contours()
 test.img_draw_contours()
 test.img_show()
