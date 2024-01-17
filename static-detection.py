055af32a69b33a4acca519d3e957f06ee31dcdaf
soggy-soup <odion1776@gmail.com>
2024-01-17 16:51:46 -0500

    code

--- static-detection.py
+++ static-detection.py
@@ -1,3 +1,38 @@
+import numpy as np
+import cv2
+
+class process_img:
+    def __init__(self, img_path):
+        self.img_read = cv2.imread(img_path)
+        self.img_in_processing = self.img_read
+
+    def img_show(self):
+        cv2.imshow("Image in Processing", self.img_in_processing)
+
+    def img_scale(self, percent = None):
+        width = int(self.img_in_processing.shape[1] * percent/100)
+        height = int(self.img_in_processing.shape[0] * percent/100)
+        dim = (width, height)
+        self.img_in_processing = cv2.resize(self.img_in_processing,dim,interpolation=cv2.INTER_AREA)
+        
+
+
+    def img_crop(self,):
+
+
+    def img_detect_contours(self,):
+
+
+    def img_draw_contours(self,):
+
+
+
+
+
+
+
+
+
 
 
 
