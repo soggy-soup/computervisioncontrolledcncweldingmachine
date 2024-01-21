3bff2bf718909ac7d939c41a320b06682decab44
soggy-soup <odion1776@gmail.com>
2024-01-21 14:08:43 -0500

    update

--- static_detection.py
+++ static_detection.py
@@ -1,5 +1,6 @@
-import numpy as np
+# import numpy as np
 import cv2
+
 
 class process_img:
     def __init__(self, img_path):
@@ -17,40 +18,41 @@ class process_img:
 
         while True:
             key = cv2.waitKey(1)
-            if key == 27: 
+            if key == 27:
                 break
 
             current_width = cv2.getWindowImageRect("Display Image")[2]
             new_height = int(current_width / aspect_ratio)
             cv2.resizeWindow("Display Image", current_width, new_height)
         cv2.destroyAllWindows()
-        
+
     def img_crop(self):
         self.img_in_processing = self.img_in_processing[175:3300, 535:3950]
-    
+
     def img_gaussian_blur(self):
         self.img_in_processing = cv2.GaussianBlur(self.img_in_processing, (3, 3), 0)
-    
+
     def img_median_blur(self):
         self.img_in_processing = cv2.medianBlur(self.img_in_processing, 5)
-    
+
     def img_bilateral_blur(self):
         self.img_in_processing = cv2.bilateralFilter(self.img_in_processing, 9, 75, 75)
 
     def img_basic_blur(self):
         self.img_in_processing = cv2.blur(self.img_in_processing, (100, 100))
 
-    def img_detect_contours(self,):
+    def img_detect_contours(self):
         self.gray = cv2.cvtColor(self.img_in_processing, cv2.COLOR_RGB2GRAY)
-        _, self.thresh = cv2.threshold(self.gray, 110, 255, cv2.THRESH_BINARY) #0-255, 0-black, 255-white https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
-        #self.thresh = cv2. adaptiveThreshold(self.gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,2)
+        _, self.thresh = cv2.threshold(self.gray, 110, 255, cv2.THRESH_BINARY)  # 0-255, 0-black, 255-white https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+        # self.thresh = cv2. adaptiveThreshold(self.gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,2)
 
         self.contours, self.heirarchy = cv2.findContours(self.thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    
+
     def img_draw_contours(self,):
         self.img_in_processing = cv2.drawContours(self.img_read[175:3300, 535:3950], self.contours, -1, (0, 255, 0), 30)
 
-test = process_img('C:/Users/odion/OneDrive/Documents/CAD Stuff/CNC Welding Table/New Computer Vision Code/computervisioncontrolledcncweldingmachine/images/othersideofplate.jpg')
+
+test = process_img("C:\\Users\\Aidan\\Documents\\Projects\\CNC Welding Table\\CNCWelderCode\\computervisioncontrolledcncweldingmachine\\images\\othersideofplate.jpg")
 test.img_crop()
 test.img_bilateral_blur()
 test.img_detect_contours()
@@ -59,14 +61,7 @@ test.img_draw_contours()
 test.img_show(test.img_in_processing)
 
 
-
-
-
-
-
-
-
-'''
+"""
     def img_show(self):
         aspect_ratio = self.img_in_processing.shape[1] / self.img_in_processing.shape[0]
         cv2.namedWindow("Display Image", cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL)
@@ -81,4 +76,4 @@ test.img_show(test.img_in_processing)
             new_height = int(current_width / aspect_ratio)
             cv2.resizeWindow("Display Image", current_width, new_height)
         cv2.destroyAllWindows()
-'''
+"""
