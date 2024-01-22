1e8a0b008ea903e951b7412d30149de7f217ce61
soggy-soup <odion1776@gmail.com>
2024-01-22 13:37:13 -0500

    comparetest

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
+import cv2
+
+
+
+
+img = cv2.imread("C:\\Users\\Aidan\\Documents\\Projects\\CNC Welding Table\\CNCWelderCode\\computervisioncontrolledcncweldingmachine\\images\\othersideofplate.jpg")
+img2 = cv2.imread("C:\\Users\\Aidan\\Documents\\Projects\\CNC Welding Table\\CNCWelderCode\\computervisioncontrolledcncweldingmachine\\images\\othersideofplate.jpg")
+combine = cv2.compare(img,img2,cv2.CMP_EQ)
+cv2.imshow("Display Image", combine)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
