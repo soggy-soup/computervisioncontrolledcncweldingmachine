03d72b1128e2f6b6be49cf8a6c1901ddb985c0be
soggy-soup <odion1776@gmail.com>
2024-01-22 10:33:40 -0500

    hsv

--- static_detection.py
+++ static_detection.py
@@ -9,6 +9,7 @@ class process_img:
         self.contours = []
         self.heirarchy = []
         self.gray = []
+        self.saturate = []
         self.thresh = []
 
     def img_show(self, img_to_show):
@@ -41,13 +42,18 @@ class process_img:
     def img_basic_blur(self):
         self.img_in_processing = cv2.blur(self.img_in_processing, (100, 100))
 
-    def img_detect_contours(self):
+    def img_detect_GRAY_contours(self):
         self.gray = cv2.cvtColor(self.img_in_processing, cv2.COLOR_RGB2GRAY)
         _, self.thresh = cv2.threshold(self.gray, 110, 255, cv2.THRESH_BINARY)  # 0-255, 0-black, 255-white https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
         # self.thresh = cv2. adaptiveThreshold(self.gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11,2)
 
         self.contours, self.heirarchy = cv2.findContours(self.thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
+    
+    def img_detect_HSV_contours(self):
+        self.saturate = cv2.cvtColor(self.img_in_processing, cv2.COLOR_RGB2HSV)
+        self.thresh = cv2.inRange(self.saturate, (0,0,105), (180,50,255))
+        self.contours, self.heirarchy = cv2.findContours(self.thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        
     def img_draw_contours(self,):
         self.img_in_processing = cv2.drawContours(self.img_read[175:3300, 535:3950], self.contours, -1, (0, 255, 0), 30)
 
@@ -55,10 +61,12 @@ class process_img:
 test = process_img("C:\\Users\\Aidan\\Documents\\Projects\\CNC Welding Table\\CNCWelderCode\\computervisioncontrolledcncweldingmachine\\images\\othersideofplate.jpg")
 test.img_crop()
 test.img_bilateral_blur()
-test.img_detect_contours()
+test.img_detect_HSV_contours()
+test.img_show(test.saturate)
 test.img_show(test.thresh)
 test.img_draw_contours()
 test.img_show(test.img_in_processing)
+print(test.heirarchy)
 
 
 """
